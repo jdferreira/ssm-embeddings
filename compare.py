eb7b1e8e2d93799376dab2b40627ba7f26153dfc
João D. Ferreira <jotomicron@gmail.com>
2021-07-07 16:22:31 +0100

    Add some new comparers
    We now can compare sets of concepts based on their path distance, and on
    whether one concept subsumes the other. This improves the pool of
    ontology characteristics that are computed for a pair of entities.

--- compare.py
+++ compare.py
@@ -27,6 +27,20 @@ class World:
 
         self.classes: list[owlready2.ThingClass] = list(ontology.classes())
 
+    def get_children_of(self, concept: owlready2.ThingClass) -> list[owlready2.ThingClass]:
+        return [
+            c
+            for c in self.ontology.get_children_of(concept)
+            if isinstance(c, owlready2.ThingClass)
+        ]
+
+    def get_parents_of(self, concept: owlready2.ThingClass) -> list[owlready2.ThingClass]:
+        return [
+            c
+            for c in self.ontology.get_parents_of(concept)
+            if isinstance(c, owlready2.ThingClass)
+        ]
+
 
 class WorldBound:
     def __init__(self):
@@ -128,7 +142,7 @@ class ICSanchez(ProbabilityBasedICCalculator):
 
                 highest_ancestor_count = max(
                     highest_ancestor_count,
-                    len(c.ancestors())
+                    len(ancestors(c))
                 )
 
         self.min_probability = 1 / highest_ancestor_count
@@ -146,6 +160,52 @@ class ICSanchez(ProbabilityBasedICCalculator):
 
     def __repr__(self):
         return 'ICSanchez()'
+
+
+class ICDepth(ICCalculator):
+    def __init__(self, min_max):
+        super().__init__(True)
+
+        self.min_max = min_max
+
+    def set_world(self, world: World):
+        super().set_world(world)
+
+        # We need to to go through all concepts. Let's find the roots and go our
+        # way down
+        todo = [
+            c
+            for c in self.world.classes
+            if len(ancestors(c)) == 1 and len(self.world.get_children_of(c)) > 1
+        ]
+
+        self.depths = {
+            c: 1
+            for c in todo
+        }
+
+        while todo:
+            c = todo.pop(0)
+
+            children_depth = self.depths[c] + 1
+
+            for child in self.world.get_children_of(c):
+                if (
+                    child not in self.depths or
+                    (self.min_max == 'min' and self.depths[child] > children_depth) or
+                    (self.min_max == 'max' and self.depths[child] < children_depth)
+                ):
+                    self.depths[child] = children_depth
+
+                    todo.append(child)
+
+        self.max_depth = max(self.depths.values())
+
+    def calculate(self, concept: owlready2.ThingClass) -> float:
+        return self.depths.get(concept, 0) / self.max_depth
+
+    def __repr__(self):
+        return 'ICDepth()'
 
 
 class Comparer(WorldBound):
@@ -191,7 +251,7 @@ class Lin(ICBasedComparer):
 
         den = self.ic_calculator(one) + self.ic_calculator(two)
 
-        return num / den
+        return num / den if den != 0 else 0
 
     def __repr__(self):
         return f'Lin({self.ic_calculator!r})'
@@ -229,7 +289,7 @@ class Rel(ICBasedComparer):
         num = 2 * ic_mica
         den = self.ic_calculator(one) + self.ic_calculator(two)
 
-        return num / den * (1 - p_mica)
+        return num / den * (1 - p_mica) if den != 0 else 0
 
     def __repr__(self):
         return f'Rel({self.ic_calculator!r})'
@@ -244,7 +304,7 @@ class Faith(ICBasedComparer):
 
         den = self.ic_calculator(one) + self.ic_calculator(two) - ic_mica
 
-        return ic_mica / den
+        return ic_mica / den if den != 0 else 0
 
     def __repr__(self):
         return f'Faith({self.ic_calculator!r})'
@@ -255,10 +315,94 @@ class CMatch(Comparer):
         common_ancestors = ancestors(one).intersection(ancestors(two))
         union_ancestors = ancestors(one).union(ancestors(two))
 
-        return len(common_ancestors) / len(union_ancestors)
+        return len(common_ancestors) / len(union_ancestors) if union_ancestors else 0
 
     def __repr__(self):
         return 'CMatch()'
+
+
+class SubsumerComparer(Comparer):
+    def compare(self, one: owlready2.ThingClass, two: owlready2.ThingClass) -> float:
+        return 1 if one in ancestors(two) or two in ancestors(one) else 0
+
+    def __repr__(self):
+        return 'SubsumerComparer()'
+
+
+class ICSubsumerComparer(ICBasedComparer):
+    def compare(self, one: owlready2.ThingClass, two: owlready2.ThingClass) -> float:
+        if one in ancestors(two) or two in ancestors(one):
+            return min(self.ic_calculator(one), self.ic_calculator(two))
+        else:
+            return 0
+
+    def __repr__(self):
+        return f'ICSubsumerComparer({self.ic_calculator!r})'
+
+
+class AncestryPathDistanceCalculator:
+    def __init__(self, world: World):
+        self.world = world
+
+        self.cache: dict[
+            owlready2.ThingClass,
+            dict[owlready2.ThingClass, int]
+        ] = {}
+
+    def calculate(self, concept: owlready2.ThingClass) -> dict[owlready2.ThingClass, int]:
+        cached = self.cache.get(concept)
+
+        if cached is not None:
+            return cached
+
+        parents = self.world.get_parents_of(concept)
+
+        ancestry_of_parents = {
+            parent: self.calculate(parent)
+            for parent in parents
+        }
+
+        result: dict[owlready2.ThingClass, int] = {concept: 0}
+
+        for ancestry in ancestry_of_parents.values():
+            for ancestor, distance in ancestry.items():
+                my_distance = distance + 1
+
+                if ancestor in result:
+                    result[ancestor] = min(result[ancestor], my_distance)
+                else:
+                    result[ancestor] = my_distance
+
+        self.cache[concept] = result
+
+        return result
+
+
+class PathLengthComparer(Comparer):
+    def set_world(self, world: World):
+        super().set_world(world)
+
+        self.ancestry_path_distance_calculator = AncestryPathDistanceCalculator(
+            world
+        )
+
+    def compare(self, one: owlready2.ThingClass, two: owlready2.ThingClass) -> float:
+        distances_one = self.ancestry_path_distance_calculator.calculate(one)
+        distances_two = self.ancestry_path_distance_calculator.calculate(two)
+
+        common = set(distances_one) & set(distances_two)
+
+        if not common:
+            return -1 # This should instead be a very large value
+        else:
+            return min(
+                distances_one[c] + distances_two[c]
+                for c in common
+            )
+
+
+    def __repr__(self):
+        return 'PathLengthComparer()'
 
 
 class SetComparer(WorldBound):
@@ -276,6 +420,10 @@ def induced_ancestors(concepts: OwlCollection) -> set[owlready2.ThingClass]:
     return {ancestor for concept in concepts for ancestor in ancestors(concept)}
 
 
+def induced_descendants(concepts: OwlCollection) -> set[owlready2.ThingClass]:
+    return {descendant for concept in concepts for descendant in concept.descendants()}
+
+
 class SimUI(SetComparer):
     def compare(self, one: OwlCollection, two: OwlCollection) -> float:
         one_ancestors = induced_ancestors(one)
@@ -284,13 +432,24 @@ class SimUI(SetComparer):
         inter = one_ancestors.intersection(two_ancestors)
         union = one_ancestors.union(two_ancestors)
 
-        if not union:
-            return 0.0
-        else:
-            return len(inter) / len(union)
+        return len(inter) / len(union) if union else 0
 
     def __repr__(self):
         return f'SimUI()'
+
+
+class ReverseSimUI(SetComparer):
+    def compare(self, one: OwlCollection, two: OwlCollection) -> float:
+        one_descendants = induced_descendants(one)
+        two_descendants = induced_descendants(two)
+
+        inter = one_descendants.intersection(two_descendants)
+        union = one_descendants.union(two_descendants)
+
+        return len(inter) / len(union) if union else 0
+
+    def __repr__(self):
+        return f'ReverseSimUI()'
 
 
 class SimGIC(SetComparer):
@@ -311,6 +470,26 @@ class SimGIC(SetComparer):
 
     def __repr__(self):
         return f'SimGIC({self.ic_calculator!r})'
+
+
+class ReverseSimGIC(SetComparer):
+    def __init__(self, ic_calculator: ICCalculator):
+        self.ic_calculator = ic_calculator
+
+    def compare(self, one: OwlCollection, two: OwlCollection) -> float:
+        one_descendants = induced_descendants(one)
+        two_descendants = induced_descendants(two)
+
+        inter = one_descendants.intersection(two_descendants)
+        union = one_descendants.union(two_descendants)
+
+        if not union:
+            return 0.0
+        else:
+            return sum(self.ic_calculator(c) for c in inter) / sum(self.ic_calculator(c) for c in union)
+
+    def __repr__(self):
+        return f'ReverseSimGIC({self.ic_calculator!r})'
 
 
 class BMA(SetComparer):
@@ -353,20 +532,43 @@ class SimilarityComputer:
 def get_all_comparers() -> list[SetComparer]:
     ic_seco = ICSeco()
     ic_sanchez = ICSanchez()
+    ic_depth_min = ICDepth('min')
+    ic_depth_max = ICDepth('max')
 
     return [
         BMA(Resnik(ic_seco)),
         BMA(Resnik(ic_sanchez)),
+        BMA(Resnik(ic_depth_min)),
+        BMA(Resnik(ic_depth_max)),
         BMA(Lin(ic_seco)),
         BMA(Lin(ic_sanchez)),
+        BMA(Lin(ic_depth_min)),
+        BMA(Lin(ic_depth_max)),
         BMA(JiangConrath(ic_seco)),
         BMA(JiangConrath(ic_sanchez)),
+        BMA(JiangConrath(ic_depth_min)),
+        BMA(JiangConrath(ic_depth_max)),
         BMA(Rel(ic_seco)),
         BMA(Rel(ic_sanchez)),
         BMA(Faith(ic_seco)),
         BMA(Faith(ic_sanchez)),
+        BMA(Faith(ic_depth_min)),
+        BMA(Faith(ic_depth_max)),
         BMA(CMatch()),
+        BMA(SubsumerComparer()),
+        BMA(ICSubsumerComparer(ic_seco)),
+        BMA(ICSubsumerComparer(ic_sanchez)),
+        BMA(ICSubsumerComparer(ic_depth_min)),
+        BMA(ICSubsumerComparer(ic_depth_max)),
+        BMA(PathLengthComparer()),
         SimUI(),
         SimGIC(ic_seco),
         SimGIC(ic_sanchez),
+        SimGIC(ic_depth_min),
+        SimGIC(ic_depth_max),
+        ReverseSimUI(),
+        ReverseSimGIC(ic_seco),
+        ReverseSimGIC(ic_sanchez),
+        ReverseSimGIC(ic_depth_min),
+        ReverseSimGIC(ic_depth_max),
     ]
